c40a04fa21087103d4520dd46b47d9c24b4a89fc
Saurabh Singh <saurabh.singh04@nagarro.com>
2020-01-07 09:53:55 +0000

    Added documentation command

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ docker-compose build --no-cache
 docker-compose up -d
 sleep 15
 docker-compose exec app php artisan migrate
+docker-compose exec app php artisan l5-swagger:generate
 docker-compose exec app ./vendor/bin/phpunit
