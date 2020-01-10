20f1a45914280110583b7db7780ee491dbe9642e
Saurabh Singh <saurabh.singh04@nagarro.com>
2020-01-10 05:52:17 +0000

    Added force command to create migration in production without interaction

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ docker-compose down -v
 docker-compose build --no-cache
 docker-compose up -d
 sleep 15
-docker-compose exec app php artisan migrate
+docker-compose exec app php artisan migrate --force
 docker-compose exec app php artisan l5-swagger:generate
 docker-compose exec app ./vendor/bin/phpunit
 docker-compose exec app bash -c "chmod -R 777 storage"
